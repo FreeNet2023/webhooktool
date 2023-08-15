f46b22d0098492c99e94cbc09a899f695897f39d
dujiang <dujiang@testin.cn>
2023-08-15 15:16:48 +0800

    add yaml

--- webhook_toolkit.py
+++ webhook_toolkit.py
@@ -1,5 +1,5 @@
 from abc import ABC
-from superagi.tools.base_tool import BaseToolkit, BaseTool, ToolConfiguration
+from superagi.tools.base_tool import BaseToolkit, BaseTool
 from typing import Type, List
 from webhook_tool import WebHookTool
 
@@ -11,5 +11,5 @@ class WebHookToolkit(BaseToolkit, ABC):
     def get_tools(self) -> List[BaseTool]:
         return [WebHookTool()]
 
-    def get_env_keys(self) -> List[ToolConfiguration]:
-        return [ToolConfiguration(key="WEBHOOK_URL", key_type=ToolConfigKeyType.STRING, is_required= True, is_secret = False)]
+    def get_env_keys(self) -> List[str]:
+        return ["WEBHOOK_URL"]
